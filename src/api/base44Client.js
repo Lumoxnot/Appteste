086dd06990195bfabd222c4458ad5b1b254dcb89
Lumoxnot <lumoxlk@gmail.com>
2026-07-20 21:38:23 -0300

    Create base44Client.js

---
+++ src/api/base44Client.js
@@ -0,0 +1,8 @@
+import { createClient } from '@base44/sdk';
+
+export const base44 = createClient({
+  appId: "6a5e6000f52d90d1feb75b81",
+  headers: {
+    "api_key": "ce79ff5dd4fe49399e08e75e4c9d81b0"
+  }
+});
